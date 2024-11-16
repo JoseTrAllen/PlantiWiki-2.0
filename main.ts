6ee3f930c700b8d32aca1135b8c0736f9894e622
JoseTA <josetrenor@gmail.com>
2024-11-16 02:33:35 +0100

    Añadidos enlaces eternos y a la página de login

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
 import { fecthPlants } from "./src/api/api";
 console.log("Hello PlantiWiki");
 /* console.log(fecthPlants()); */
+
+const login = document.getElementById("login");
+
+const loginPage = () => {
+  window.location.href = "./src/login/index.html";
+};
+
+login?.addEventListener("click", () => {
+  console.log("Funciono");
+  loginPage();
+});
