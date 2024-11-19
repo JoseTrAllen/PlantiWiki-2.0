bd8441fea71eb35a3432c4567e82c6dedd054ea9
JoseTA <josetrenor@gmail.com>
2024-11-19 17:58:41 +0100

    Cambios de estilos varios

--- main.ts
+++ main.ts
@@ -12,3 +12,38 @@ login?.addEventListener("click", () => {
   console.log("Funciono");
   loginPage();
 });
+
+let lastScrollY = window.scrollY; // initial scroll position
+
+window.addEventListener("scroll", () => {
+  const div = document.getElementById("hide-menu-div");
+  const hamburger = document.querySelector(".hamburger-div");
+  if (window.scrollY > lastScrollY) {
+    if (
+      div !== null &&
+      div !== undefined &&
+      div &&
+      hamburger instanceof HTMLDivElement
+    ) {
+      div.style.display = "none";
+      hamburger.style.display = "flex";
+    }
+  } else {
+    if (
+      div !== null &&
+      div !== undefined &&
+      div &&
+      hamburger instanceof HTMLDivElement
+    ) {
+      div.style.display = "flex";
+      hamburger.style.display = "none";
+    }
+  }
+});
+
+const hamburgerButton = document.querySelector(".hamburger-svc");
+const hamburgerMenu = document.querySelector(".toogle-menu");
+
+hamburgerButton?.addEventListener("click", () => {
+  hamburgerMenu?.classList.toggle("active");
+});
