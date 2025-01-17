452e41c772e77c8d2b0bfb15104cf00a246ee8de
JoseTA <josetrenor@gmail.com>
2025-01-17 18:52:20 +0100

    cambios

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,12 @@ export default defineConfig({
       "/api": "http://localhost:3000", // Esto redirige '/api' a la API local
     },
   },
+  build: {
+    outDir: "dist", // Asegúrate de que el directorio de salida sea correcto
+    rollupOptions: {
+      output: {
+        format: "es", // Asegúrate de que se esté utilizando un formato de módulo adecuado
+      },
+    },
+  },
 });
